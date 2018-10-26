23305bfab86ee0641d9cc9ba15f92d00857070f9
Unknown <angeldaviddeborjagarcia@gmail.com>
2018-10-26 09:26:33 +0200

    26/10/2018
    Equipos NBA

---
+++ EquiposNBA/tabla_equipos.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adeborja
+ * Date: 26/10/18
+ * Time: 8:51
+ */
+
+namespace constantesBBDD;
+class tabla_equipos
+{
+    const ID = "id";
+    const NOMBRE = "nombre";
+    const NUMERO_JUGADORES = "numeroJugadores";
+}
---
+++ EquiposNBA/tabla_jugadores.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adeborja
+ * Date: 26/10/18
+ * Time: 8:54
+ */
+
+namespace constantesBBDD;
+class tabla_jugadores
+{
+    const ID = "id";
+    const ID_EQUIPO = "id_Equipo";
+    const NOMBRE = "nombre";
+    const APELLIDOS = "apellidos";
+    const EDAD = "edad";
+    const FOTO = "foto";
+}
